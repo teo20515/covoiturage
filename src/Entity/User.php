065c578de93f10ab92dea2ab4ffff435b746210b
Stéphane Bonhomme <stephane@exselt.com>
2020-02-12 10:27:23 +0100

    implement user name

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,8 +16,25 @@ class User
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
     }
 }
