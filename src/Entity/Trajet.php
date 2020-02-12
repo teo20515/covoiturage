556a48cf5f6aea88ee45077216240a9c5ba3359d
Stéphane Bonhomme <stephane@exselt.com>
2020-02-12 11:31:17 +0100

    entity trajet

---
+++ src/Entity/Trajet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TrajetRepository")
+ */
+class Trajet
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
